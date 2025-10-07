a77fbc82416c0af7f56ade7846933bc6112d08d6
Sahil shaikh <sahilsmt741@gmail.com>
2025-10-07 20:38:58 +0000

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// This ensures paths work for GitHub Pages and locally
 export default defineConfig({
   plugins: [react()],
-  base: "/BookMyTurf/", // ✅ EXACTLY this — matches repo name
+  base: process.env.NODE_ENV === "production" ? "/BookMyTurf/" : "/",
 });
