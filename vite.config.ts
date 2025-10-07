ced9b006d794cd458476266e2e98a8286420d8c9
Sahil shaikh <sahilsmt741@gmail.com>
2025-10-07 19:59:51 +0000

    Added

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/BookMyTurf/", // 👈 this is required for GitHub Pages
+  base: "/BookMyTurf/",
 });
