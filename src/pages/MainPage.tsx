d4e5adc6db8457cc33b6feafccd25cc01c0c4908
Sahil shaikh <sahilsmt741@gmail.com>
2025-10-07 21:54:20 +0000

    change

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import football from "/football.jpg";
-import cricket from "/cricket.jpg";
-import celebration from "/celebration.jpg";
+import football from "../assets/football.jpg";
+import cricket from "../assets/cricket.jpg";
+import celebration from "../assets/celebration.jpg";
 
 const images = [football, cricket, celebration];
 
